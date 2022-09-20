36e0b8d43811ff42d9d1448371472630a44babb2
Krzysztof Krakowiak <komumor@wp.pl>
2022-09-20 14:04:24 +0200

    Updates

---
+++ precache-manifest.3e22fceeb6314620bf5f0e993dda0bec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d57d7bcdaffca9345d93",
+    "url": "/MovieSearcher/static/css/main.5d495132.chunk.css"
+  },
+  {
+    "revision": "d57d7bcdaffca9345d93",
+    "url": "/MovieSearcher/static/js/main.7770eab6.chunk.js"
+  },
+  {
+    "revision": "d0e1d4686ec1585ea9f5",
+    "url": "/MovieSearcher/static/js/runtime~main.179fcfff.js"
+  },
+  {
+    "revision": "a02664e0b39859221ec2",
+    "url": "/MovieSearcher/static/js/2.d011160c.chunk.js"
+  },
+  {
+    "revision": "e0f2b2d8720adc4b5190f977d8517174",
+    "url": "/MovieSearcher/index.html"
+  }
+];
